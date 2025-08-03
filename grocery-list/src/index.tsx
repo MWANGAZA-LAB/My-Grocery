cc3a37aa6a755be3f4d68fa954595c79dc044b51
MWANGAZA-LAB <mwanga02717@gmail.com>
2025-08-03 11:23:32 +0300

    fix: resolve duplicate React import causing deployment failure

--- grocery-list/src/index.tsx
+++ grocery-list/src/index.tsx
@@ -1,15 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.step2';
-import reportWebVitals from './reportWebVitals';
-// import { initPerformanceMonitoring } from './utils/performance';
-
-const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
 import reportWebVitals from './reportWebVitals';
 // import { initPerformanceMonitoring } from './utils/performance';
 
